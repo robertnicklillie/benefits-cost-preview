c28e99d26fe35554f104392d5803932739a34075
Nick Lillie <r.nick.lillie@gmail.com>
2021-11-24 10:21:59 -0500

    initial commit

---
+++ benefits-cost-preview/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace benefits_cost_preview.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
